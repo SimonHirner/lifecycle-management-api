94314002351e57603c90ec23f8ccca7cb884c6ad
Simon Hirner <simon.hirner@hm.edu>
2021-02-28 12:25:15 +0100

    Datenbank angepasst

--- Context.cs
+++ Context.cs
@@ -18,9 +18,14 @@ namespace LifecycleManagementAPI
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<Model>()
-                .HasMany(c => c.Devices)
-                .WithOne(a => a.Model)
-                .HasForeignKey(a => a.ModelId);
+                .HasMany(m => m.Devices)
+                .WithOne(d => d.Model)
+                .HasForeignKey(d => d.ModelId);
+
+            builder.Entity<Activity>()
+                .HasMany(a => a.Devices)
+                .WithOne(d => d.Activity)
+                .HasForeignKey(d => d.ActivityId);
         }
 
         /// <summary>
@@ -32,6 +37,56 @@ namespace LifecycleManagementAPI
         /// database table Devices
         /// </summary>
         public DbSet<Device> Devices { get; set; }
+
+        /// <summary>
+        /// database table Employees
+        /// </summary>
+        public DbSet<Employee> Employees { get; set; }
+
+        /// <summary>
+        /// database table Activities
+        /// </summary>
+        public DbSet<Activity> Activities { get; set; }
+
+        /// <summary>
+        /// database table Laptops
+        /// </summary>
+        public DbSet<Laptop> Laptops { get; set; }
+
+        /// <summary>
+        /// database table Phones
+        /// </summary>
+        public DbSet<Phone> Phones { get; set; }
+
+        /// <summary>
+        /// database table Servers
+        /// </summary>
+        public DbSet<Server> Servers { get; set; }
+
+        /// <summary>
+        /// database table Workstations
+        /// </summary>
+        public DbSet<Workstation> Workstations { get; set; }
+
+        /// <summary>
+        /// database table Disposals
+        /// </summary>
+        public DbSet<Disposal> Disposals { get; set; }
+
+        /// <summary>
+        /// database table Maintenances
+        /// </summary>
+        public DbSet<Maintenance> Maintenances { get; set; }
+
+        /// <summary>
+        /// database table Operations
+        /// </summary>
+        public DbSet<Operation> Operations { get; set; }
+        
+        /// <summary>
+        /// database table Stocks
+        /// </summary>
+        public DbSet<Stock> Stocks { get; set; }
     }
 
 }
